a7ed578d64d82ed09e7448b08b6a3b9771e29756
ramos-vini <ramoscontrole@gmail.com>
2023-10-07 17:06:05 +0200

    string slicing

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import math
+name = "Bro Code"
 
-n1 = 3.4
-n2 = 3.5
-n3 = 3.6
+# Indexing: [start:stop:step]
+print(name[4:])
+print(name[:5])
+print(name[::2])
+print(name[2:6:1])
+print(name[::-1])
 
-print(round(n1))
-print(math.ceil(n1))
-print(math.floor(n1))
-print("\n")
+# slice()
 
-print("\n")
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
 
-print(round(n3))
-print(math.ceil(n3))
-print(math.floor(n3))
-print("\n")
+websiteSlice = slice(7, -4)
 
-print(min(n1, n2, n3))
-print(max(n1, n2, n3))
+print(website1[websiteSlice])
+print(website2[websiteSlice])
