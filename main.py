89e34795c1d63e0c358e042d7645c0d93561900c
ramos-vini <ramoscontrole@gmail.com>
2023-10-07 16:48:28 +0200

    math functions

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-name = input("What is your name?")
-age = int(input("How old are you?"))
-height = float(input("How tall are you? (don't lie, buddy)"))
+import math
 
-print("Your name is " + name)
-print("You are " + str(age) + " years old")
-print("You are " + str(height) + "m tall (supposedly)")
+n1 = 3.4
+n2 = 3.5
+n3 = 3.6
+
+print(round(n1))
+print(math.ceil(n1))
+print(math.floor(n1))
+print("\n")
+
+print("\n")
+
+print(round(n3))
+print(math.ceil(n3))
+print(math.floor(n3))
+print("\n")
+
+print(min(n1, n2, n3))
+print(max(n1, n2, n3))
