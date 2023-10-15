7b984b554bb363e83637cbf19b4e0fc8131347cc
ramos-vini <ramoscontrole@gmail.com>
2023-10-15 13:51:23 +0200

    Testing Git

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-while True:
-    name = input("What's your name?")
-    if name != "":
-        break
-
-print("Hello " + name)
-
-phoneNumber = "123-456-7890"
-
-for i in phoneNumber:
-    if i == "-":
-        continue
-    print(i, end="")
+print("Testing Git")
