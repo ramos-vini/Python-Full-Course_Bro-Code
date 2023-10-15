46cb5054e3fef5abe129477ad6798a541ad2d30c
ramos-vini <ramoscontrole@gmail.com>
2023-10-15 14:01:28 +0200

    list

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Testing Git")
+foods = ["pizza", "sushi", "hamburger", "hotdog"]
+
+print(foods)
+
+foods.append("kebab")
+foods.remove("pizza")
+foods.sort()
+
+for food in foods:
+    print(food)
