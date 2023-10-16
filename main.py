b261d414d70d96bd659bd5804030b6295215ad7c
ramos-vini <ramoscontrole@gmail.com>
2023-10-16 23:07:11 +0200

    format function

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-def print_name(**names):
-    print("Hello, ", end=" ")
-    for key, value in names.items():
-        print(value, end=" ")
+print("The {:^10} jumps over the {}".format("cow", "moon"))
 
+print("The number {0} in binary is {0:b}".format(7))
 
-print_name(first="billy", middle="the burger", last="ocean")
+print(f"The number {7} in binary is {7: b}")
 
-
-
-
-
+pi = 3.14159
+print(f"PI: {pi: >10.2f}")
