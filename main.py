ec1666569f8f6a118d401153d6c7c46a94bf851c
ramos-vini <ramoscontrole@gmail.com>
2023-10-18 16:46:36 +0200

    copying files

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-text = "This is just some random text.\n"
+import shutil
 
-try:
-    with open("write.txt", "w") as write_file: # This one gets overwritten
-        write_file.write(text)
+shutil.copyfile("text.txt", "copy.txt")
 
-except Exception as e:
-    print(e)
-
-try:
-    with open("append.txt", "a") as append_file:
-        append_file.write(text)
-
-except Exception as e:
-    print(e)
 
