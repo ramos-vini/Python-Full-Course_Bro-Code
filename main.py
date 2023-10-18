77f26f7623051961b4dd5dffc00daaf3356c23e8
ramos-vini <ramoscontrole@gmail.com>
2023-10-18 11:53:53 +0200

    writing files

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+text = "This is just some random text.\n"
+
 try:
-    with open("test.txt") as file:
-        print(file.read())
-except FileNotFoundError as e:
+    with open("write.txt", "w") as write_file: # This one gets overwritten
+        write_file.write(text)
+
+except Exception as e:
     print(e)
+
+try:
+    with open("append.txt", "a") as append_file:
+        append_file.write(text)
+
 except Exception as e:
     print(e)
 
