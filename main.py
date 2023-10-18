ba4a9cc21cdff1ccf196b583cbd408a37be1810a
ramos-vini <ramoscontrole@gmail.com>
2023-10-18 10:49:28 +0200

    random object

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-print("The {:^10} jumps over the {}".format("cow", "moon"))
+import random
 
-print("The number {0} in binary is {0:b}".format(7))
+print(random.randint(1, 6))
 
-print(f"The number {7} in binary is {7: b}")
+print(random.random())
 
-pi = 3.14159
-print(f"PI: {pi: >10.2f}")
+options = ["rock", "paper", "scissors"]
+# print(type(options))
+
+print(random.choice(options))
+
+sequence = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+random.shuffle(sequence)
+
+print(sequence)
+
