0609030fd20b8d802ab82c06b3bc8833ed71a40d
ramos-vini <ramoscontrole@gmail.com>
2023-10-18 17:12:57 +0200

    moving files

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import shutil
+import os
 
-shutil.copyfile("text.txt", "copy.txt")
+src = "text.txt"
+destination = "/Users/viniciusramos/Desktop/text.txt"
+
+try:
+    if os.path.exists(destination):
+        print("This path already exists in the destination.")
+    else:
+        os.replace(src, destination)
+        print(f"'{src}' was successfully moved.")
+
+except FileNotFoundError as e:
+    print(e)
+except Exception as e:
+    print(e)
 
 
