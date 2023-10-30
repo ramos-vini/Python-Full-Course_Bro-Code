06f53ac1f0bf74a42eb6dae243f334c76ebfda65
ramos-vini <ramoscontrole@gmail.com>
2023-10-30 22:38:16 +0100

    walrus operator

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-from abc import ABC, abstractmethod
+foods = []
 
+while (food := input("What food do you like? ")) != "quit":
+    foods.append(food)
 
-class Vehicle(ABC):
-    @abstractmethod
-    def go(self):
-        pass
-
-
-class Car(Vehicle):
-    def go(self):
-        print("This car goes.")
-
-
-class Motorcycle(Vehicle):
-    def go(self):
-        print("This motorcycle goes.")
-
-
-car = Car()
-car.go()
-
-motorcycle = Motorcycle()
-motorcycle.go()
+print(foods)
