bf58848df3d8c8b1cb21db220eb3a108be5c915b
ramos-vini <ramoscontrole@gmail.com>
2023-10-31 17:05:26 +0100

    sort function

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-multiply = lambda x, y: x * y
+# my_list = ["belly", "sugar", "petota"]
+#
+# my_tuple = ("belly", "sugar", "petota")
+#
+# my_list.sort()
+#
+# for i in my_list:
+#     print(i)
+#
+# sorted_tuple = sorted(my_tuple)
+#
+# for i in sorted_tuple:
+#     print(i)
 
-print(multiply(3, 4))
+tuples_list = [
+    ("Belly", "C", 18),
+    ("Sugar", "A", 22),
+    ("Petota", "F", 36)
+]
 
+grade_key = lambda grade: grade[1]
+
+tuples_list.sort(key=grade_key)
+
+for i in tuples_list:
+    print(i)
