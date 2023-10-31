4ddcb657fafeca005ff82d20d9b619c42111d50d
ramos-vini <ramoscontrole@gmail.com>
2023-10-31 17:15:40 +0100

    map function

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-# my_list = ["belly", "sugar", "petota"]
-#
-# my_tuple = ("belly", "sugar", "petota")
-#
-# my_list.sort()
-#
-# for i in my_list:
-#     print(i)
-#
-# sorted_tuple = sorted(my_tuple)
-#
-# for i in sorted_tuple:
-#     print(i)
-
-tuples_list = [
-    ("Belly", "C", 18),
-    ("Sugar", "A", 22),
-    ("Petota", "F", 36)
+store = [
+    ("shirt", 10.0),
+    ("shoes", 18.00),
+    ("socks", 6.50)
 ]
 
-grade_key = lambda grade: grade[1]
+to_euros = lambda data: (data[0], data[1] * 0.95)
 
-tuples_list.sort(key=grade_key)
+store_euros = map(to_euros, store)
 
-for i in tuples_list:
+for i in store_euros:
     print(i)
+
