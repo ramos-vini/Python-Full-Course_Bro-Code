0550ee89df274fc2ca9aa1135cc22103a0afd297
ramos-vini <ramoscontrole@gmail.com>
2023-11-01 23:22:51 +0100

    list comprehensions

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import functools
+numbers = []
 
-numbers = [5, 4, 3, 2, 1]
+for i in range(1, 11):
+    numbers.append(i)
 
-factorial = functools.reduce(lambda x, y: x * y, numbers)
+print(numbers)
 
-print(factorial)
+numbers2 = [i for i in range(1, 11)]
+print(numbers2)
+
+numbers_even = list(filter(lambda i: i % 2 == 0, numbers))
+print(numbers_even)
+
+numbers_odd = [i for i in range(1, 11) if i % 2 != 0]
+print(numbers_odd)
+
+numbers_odd_x = [i if i % 2 != 0 else "X" for i in range(1, 11)]
+print(numbers_odd_x)
 
