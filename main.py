c046b756a9746868f0ced82c29cc96c018a62c66
ramos-vini <ramoscontrole@gmail.com>
2023-11-02 22:35:28 +0100

    zip function

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-cities_temp = {"Rio": 34, "Berlin": 13, "Madrid": 21, "Denmark": 7}
-print(cities_temp)
+usernames = ["billy", "johnson", "petodas"]
 
-cities_temp_fahrenheit = {key: round(value/5*9+32) for (key, value) in cities_temp.items()}
-print(cities_temp_fahrenheit)
+passwords = ["billy_matato", "sonjohn", "potatoes"]
 
-warm_cities_temp = {key: value for (key, value) in cities_temp.items() if value >= 20}
-print(warm_cities_temp)
+users = dict(zip(usernames, passwords))
 
-cities_temp_desc = {key: "Acceptable" if value >= 10 else "No way" for (key,value) in cities_temp.items()}
-print(cities_temp_desc)
+print(type(users))
 
-
-def check_temp(value):
-    if value >= 30:
-        return "Too hot"
-    elif value >= 20:
-        return "Good hot"
-    elif value >= 10:
-        return "Nice cold"
-    else:
-        return "Absolutely freezing"
-
-
-cities_temp_desc_2 = {key: check_temp(value) for (key, value) in cities_temp.items()}
-print(cities_temp_desc_2)
+for (key, value) in users.items():
+    print(key + " : " + value)
