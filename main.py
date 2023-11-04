1e5e33b29d0f20911288c8ca7d196d5a113c2d3d
ramos-vini <ramoscontrole@gmail.com>
2023-11-04 23:20:19 +0100

    multiprocessing

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
-import threading
+from multiprocessing import Process, cpu_count
 import time
 
 
-def time_counter():
-    print()
-    print()
+def number_counter(num):
     counter = 0
-    while True:
-        time.sleep(1)
+    while counter < num:
         counter += 1
-        print(f"Your program has been running for {counter} seconds")
 
 
-x = threading.Thread(target=time_counter, daemon=True)
-x.start()
+def main():
+    start_time = time.perf_counter()
+    a = Process(target=number_counter(100000))
+    b = Process(target=number_counter(100000))
+    c = Process(target=number_counter(100000))
+    d = Process(target=number_counter(100000))
+    e = Process(target=number_counter(100000))
+    f = Process(target=number_counter(100000))
+    g = Process(target=number_counter(100000))
+    h = Process(target=number_counter(100000))
+    i = Process(target=number_counter(100000))
+    j = Process(target=number_counter(100000))
 
-answer = input("Type in anything in order to exit the program: \n")
+    a.start()
+    b.start()
+    c.start()
+    d.start()
+    e.start()
+    f.start()
+    g.start()
+    h.start()
+    i.start()
+    j.start()
+
+    end_time = time.perf_counter()
+
+    print(f"CPU cores: {cpu_count()}")
+    total_time = end_time - start_time
+    print(f"Program runned in {total_time} seconds")
+
+
+if __name__ == "__main__":
+    main()
 
 
